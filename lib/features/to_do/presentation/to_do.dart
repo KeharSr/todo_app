03fc90367f8fc1948779f88c65b95f7153348888
srKehar <keharojha@veloxies.com>
2025-12-04 19:25:15 +0545

    feat: adding the ui

--- lib/features/to_do/presentation/to_do.dart
+++ lib/features/to_do/presentation/to_do.dart
@@ -1,10 +1,98 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class Todo extends StatelessWidget {
   const Todo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Container(
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              offset: Offset(0, 1),
+
+              color: Colors.red.shade200,
+            ),
+          ],
+          borderRadius: BorderRadius.circular(16),
+          shape: BoxShape.rectangle,
+          color: Colors.deepOrange,
+        ),
+        child: Icon(color: Colors.white, Icons.add, size: 40),
+      ),
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15, top: 20, right: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  "${DateFormat.EEEE().format(DateTime.now())},",
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+                Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(CupertinoIcons.search, color: Colors.deepOrange),
+                ),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(CupertinoIcons.bell, color: Colors.deepOrange),
+                    ),
+                    Positioned(
+                      left: 26,
+                      bottom: 32,
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        decoration: BoxDecoration(
+                          color: Colors.deepOrange,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(
+                          child: Text(
+                            '+2',
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Text(
+              DateFormat('d MMMM yyyy').format(DateTime.now()),
+              style: GoogleFonts.poppins(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
